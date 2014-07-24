d082ed74e2ab97470f8f328b7cb707dcefd7a2e2
Jonathan Blake <jonawayneblake@gmail.com>
2014-07-23 20:59:05 -0400

    initial project version: first commit

---
+++ ThumbTack/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThumbTack::Application.config.secret_token = '02b103cbe83755ceea36b380c808ee7bde6e235cc7bfbb2ecb3c1b7b6cf60a3a414620e91e938da8c5bbe30895b5554d94fb066493424bd30ff3d608ceaa73c9'
